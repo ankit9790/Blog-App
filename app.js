976e476e02f413cb0ec17ead0987c6d741303780
ankit <ankity9790@gmail.com>
2025-11-24 17:39:31 +0530

    fixing bug

--- app.js
+++ app.js
@@ -17,8 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then((e) => console.log("Connected to MongoDB"));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
